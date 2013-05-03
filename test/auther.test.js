9069eab87cf1a978a5422bd10a64f9d13b75b44a
anderslarsson <anders.larsson.funkis@gmail.com>
2013-05-03 22:16:55 +0200

    resource ids can be strings or nubmers

--- test/auther.test.js
+++ test/auther.test.js
@@ -64,6 +64,9 @@ describe('Auther test suite', function() {
 		it('should return true if id is in parameter list', function() {
 			auther.hasAccess({ cid: 34 }, { session: { user: { AOHash: { cid : [1,2,34,40] }}}}).should.eql(true)
 		})
+		it('should return true if id as string is in parameter list', function() {
+			auther.hasAccess({ cid: 34 }, { session: { user: { AOHash: { cid : [1,2,'34',40] }}}}).should.eql(true)
+		})
 		it('should return false if id is not in parameter list', function() {
 			auther.hasAccess({ cid: 34 }, { session: { user: { AOHash: { cid : [1,2,40] }}}}).should.eql(false)
 		})
